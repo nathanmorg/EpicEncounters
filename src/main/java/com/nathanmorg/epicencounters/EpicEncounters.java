5d0e7d244e2107da2173844827837ae5d6c2d21d
nathan <116317850+nathanmorg@users.noreply.github.com>
2023-07-23 18:39:26 -0500

    Initial Commit

---
+++ src/main/java/com/nathanmorg/epicencounters/EpicEncounters.java
@@ -0,0 +1,17 @@
+package com.nathanmorg.epicencounters;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EpicEncounters extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
